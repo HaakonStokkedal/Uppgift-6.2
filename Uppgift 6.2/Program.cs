dfbacf06d6e725abedce805386862b3ccec82dc6
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-02-28 12:40:14 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Uppgift_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal:");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett till heltal:");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{StörstaTal(tal1, tal2)} är det största talet.");
+
+            Console.ReadKey();
+        }
+
+        static int StörstaTal(int tal1, int tal2)
+        {
+            int störstaTal = tal1;
+
+            if (tal1 < tal2)
+            {
+                störstaTal = tal2;
+            }
+            return störstaTal;
+        }
+    }
+}
